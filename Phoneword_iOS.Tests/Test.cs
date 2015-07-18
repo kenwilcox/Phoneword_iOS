30d07728e2e5b3584b9ecbeff25f7490dc6860ea
Ken Wilcox <wilcoxk@gmail.com>
2015-07-18 01:19:21 -0600

    Trying out testing
    But my trial expired the same day I got it, ugh.

---
+++ Phoneword_iOS.Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Phoneword_iOS.Tests
+{
+  [TestFixture()]
+  public class Test
+  {
+    [Test()]
+    public void TestCase()
+    {
+      var number = PhoneTranslator.ToNumber("1234567890");
+      Assert.AreEqual("1234567890", number);
+    }
+  }
+}
+
